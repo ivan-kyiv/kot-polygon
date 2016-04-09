0c53e7e2af769fc5e38de4e88e9e7443c114524e
ivan-kyiv <pushivan53@gmail.com>
2016-04-09 17:25:09 +0300

    + functions find_X and dot_cuts

--- solution.js
+++ solution.js
@@ -1,16 +1,17 @@
 var points = {  // All points we have. Format {ID:{x:X, y:Y}, ...}
   last: 0,      // Last used ID
   insert: function(new_x, new_y) {  // Insert new point
+  // returns id of added or exist point
     for (var i = 1; i <= this.last; i++) {
       if (this[i].x == new_x && this[i].y == new_y) {
         return i
-        }
       }
+    }
     this.last++;
     this[this.last] = {x: new_x, y: new_y};
     return [this.last]
-    }
   }
+}
 
 var sections = {}; // List by names of arrays of line sections.
 // Format {NAME:[[begin_point_ID, end_point_ID, state], ...], ...}
@@ -34,11 +35,15 @@ function p_import(p_name, vertexes) {
     sections[p_name][ip-1][1] = c_p;
     sections[p_name][ip] = [c_p, null, 0];
     ip++;
-    }
-  sections[p_name][ip-1][1] = b_p;
   }
+  sections[p_name][ip-1][1] = b_p;
+}
+
+var rel = {} // relative masures of four points for functions _X and find_X
+  // Format: rel[base point][related point or vector product]
 
 function _X(b1, e1, b2, e2) {
+// rewrites global variable rel
 // returns intersection mode of two line segments
 // arguments are four point in format {x:X, y:Y}:
 //   1, 2 - numbers of segments
@@ -50,15 +55,13 @@ function _X(b1, e1, b2, e2) {
 //       (if mutual is one of these four)
 //   'stuck' - collinear
 //   'X' - beautiful intersection :) not like above
-  var
-    rel = { // relative masures of these four points
-    // Format: rel[base point][related point or vector product]
-      b1: {
-        e1: {x:(e1.x - b1.x), y:(e1.y - b1.y)},
-        b2: {x:(b2.x - b1.x), y:(b2.y - b1.y)},
-        e2: {x:(e2.x - b1.x), y:(e2.y - b1.y)}
-      }
+  rel = {
+    b1: {
+      e1: {x:(e1.x - b1.x), y:(e1.y - b1.y)},
+      b2: {x:(b2.x - b1.x), y:(b2.y - b1.y)},
+      e2: {x:(e2.x - b1.x), y:(e2.y - b1.y)}
     }
+  }
   rel.b1.zb = rel.b1.b2.x * rel.b1.e1.y - rel.b1.b2.y * rel.b1.e1.x;
   rel.b1.ze = rel.b1.e2.x * rel.b1.e1.y - rel.b1.e2.y * rel.b1.e1.x;
   rel.b1.zz = rel.b1.zb * rel.b1.ze;
@@ -71,7 +74,8 @@ function _X(b1, e1, b2, e2) {
   rel.b2 = {
     b1: {x:(b1.x - b2.x), y:(b1.y - b2.y)},
     e1: {x:(e1.x - b2.x), y:(e1.y - b2.y)},
-    e2: {x:(e2.x - b2.x), y:(e2.y - b2.y)}
+    e2: {x:(e2.x - b2.x), y:(e2.y - b2.y)},
+    base: (b2)         // This is only for 'find_X' function
   }
   rel.b2.zb = rel.b2.b1.x * rel.b2.e2.y - rel.b2.b1.y * rel.b2.e2.x;
   rel.b2.ze = rel.b2.e1.x * rel.b2.e2.y - rel.b2.e1.y * rel.b2.e2.x;
@@ -92,9 +96,34 @@ function _X(b1, e1, b2, e2) {
   }
   // Now I khow that the line segment 2 abuts the line 1
   if (rel.b2.zz == 0) {
-    return 'miss' //
+    return 'miss'      // The segments are coupled
   }
-  return rel.b1.zz
+  // Now I know that the line segment 2 abuts the line segment 1
+  if (rel.b1.zb == 0) {
+    return 'b2'
+  }
+  return 'e2'
+}
+
+function find_X() {
+// Can be used olny if '_X' results 'X'
+// Reads rel
+// returns intersection point (see '_X' function)
+  var ratio = Math.abs(rel.b1.zb/(rel.b1.zb - rel.b1.ze));
+  return {
+    x: rel.b2.base.x + ratio * rel.b2.e2.x,
+    y: rel.b2.base.y + ratio * rel.b2.e2.y
+  }
+}
+
+function dot_cuts(p_name, sect, pid) {
+// Cuts the sect line segment at the p_name polygon by the pid point
+  sections[p_name][sections[p_name].length] = [
+    pid,
+    sections[p_name][sect][1],
+    0
+  ];
+  sections[p_name][sect][1] = pid
 }
 
 function intersects(fig1, fig2) {
